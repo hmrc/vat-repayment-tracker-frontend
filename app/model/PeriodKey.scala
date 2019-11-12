d212eddd579f7168b7524cddfbe476eba53ecb46
andrewrichards <andrew.richards1@digital.hmrc.gov.uk>
2019-11-12 14:59:04 +0000

    [AJER] [OPS-3679] PRA Changes

--- app/model/PeriodKey.scala
+++ app/model/PeriodKey.scala
@@ -16,13 +16,10 @@
 
 package model
 
-import java.time.Period
-
 import controllers.ValueClassBinder.valueClassBinder
 import play.api.libs.functional.syntax._
 import play.api.libs.json.Format
 import play.api.mvc.PathBindable
-import play.utils.UriEncoding
 
 final case class PeriodKey(value: String)
 
@@ -30,14 +27,5 @@ object PeriodKey {
 
   implicit val format: Format[PeriodKey] = implicitly[Format[String]].inmap(PeriodKey(_), _.value)
   implicit val vrnBinder: PathBindable[PeriodKey] = valueClassBinder(_.value)
-
-  //  def escapePeriodKeyHash(periodKey:PeriodKey) = {
-  //    if (periodKey.value.length != 4) throw new RuntimeException(s"Invalid Length period key ${periodKey.value}")
-  //
-  //    if (periodKey.value.startsWith("#")) {
-  //      encode
-  //    }
-  //    else periodKey
-  //  }
 
 }
