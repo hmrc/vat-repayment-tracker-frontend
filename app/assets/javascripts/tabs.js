e9e7f1f529768897a6bba51982f8eb2586102f72
Olabode Alarapon <15871275+akure4life@users.noreply.github.com>
2021-01-25 16:09:40 +0000

    [OA][OPS-5836] Error message multiple issues fixed

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -49,7 +49,6 @@
     var tabSelectedClass = 'govuk-tabs__tab--selected'
     var panelHiddenClass = 'govuk-tabs__panel--hidden'
     var listItemSelectedClass = 'govuk-tabs__list-item--selected'
-    var listItem = d.querySelectorAll('.govuk-tabs__list-item')
 
     Array.prototype.forEach.call(tabs, function (tabsEl, i) {
 
@@ -66,15 +65,6 @@
 
       })
 
-     Array.prototype.forEach.call(tabLinks, function (listItem) {
-
-       //setup event listener for list items
-       listItem.addEventListener('click', function(event) {
-         event.preventDefault()
-         tabClick(this)
-       })
-
-     })
 
       function tabClick(tab) {
 
@@ -82,13 +72,12 @@
         //change aria-selected to false for all tabs
         Array.prototype.forEach.call(tabLinks, function (tabLink) {
           removeClass(tabLink, tabSelectedClass)
+          removeClass(tabLink.closest('li') listItemSelectedClass)
           tabLink.setAttribute('aria-selected', 'false')
           tabLink.setAttribute('tabindex', '-1')
         })
 
-        Array.prototype.forEach.call(listItem, function (listItem) {
-          removeClass(listItem, listItemSelectedClass)
-        })
+
 
         //add selected class to the selected tab
         //change aria-selected to true for the selected tab
