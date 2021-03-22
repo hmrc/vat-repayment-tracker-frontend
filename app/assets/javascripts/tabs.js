83027c71ab46c3d94f2cc6c48df6a4273648845a
Olabode Alarapon <15871275+Akure4Life@users.noreply.github.com>
2021-03-22 09:31:46 +0000

    Ops 6061 (#169)
    * [OA][OPS-6061] Tabs do not follow pattern fixed
    
    # Conflicts:
    #	app/assets/javascripts/tabs.js
    
    * [OA][OPS-6061] Tabs do not follow pattern fixed

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -43,8 +43,7 @@
     var mq = w.matchMedia(mobileMediaQuery)
     if(!mq.matches) {
         jsTabs()
-
-      }
+    }
   })
 
   function jsTabs() {
@@ -63,69 +62,34 @@
 
         //setup event listener for tabs
         tabLink.addEventListener('click', function(event) {
+          console.log(window.devicePixelRatio)
+          if(window.devicePixelRatio < 2) {
           event.preventDefault()
           tabClick(this)
+          }
         })
 
         tabLink.addEventListener('keydown', function(event) {
-            var tabKeyDownFirst = d.getElementsByClassName('govuk-tabs__tab').item(0);
-            var tabKeyDownSecond = d.getElementsByClassName('govuk-tabs__tab').item(1);
-            var paneKeyDownFirst  = d.getElementsByClassName('govuk-tabs__panel').item(0);
-            var paneKeyDownSecond  = d.getElementsByClassName('govuk-tabs__panel').item(1);
-            switch (event.keyCode) {
-                case 37:
-                    Array.prototype.forEach.call(tabLinks, function (tabLink) {
-                        removeClass(tabLink, tabSelectedClass)
-                        removeClass(tabLink.parentNode, listItemSelectedClass)
-                        tabLink.setAttribute('aria-selected', 'false')
-                        tabLink.setAttribute('tabindex', '-1')
-                        tabLink.setAttribute('role', 'tab')
-                    })
-                    addClass(tabKeyDownFirst, tabSelectedClass)
-                    tabKeyDownFirst.setAttribute('aria-selected', 'true')
-                    tabKeyDownFirst.setAttribute('tabindex', '0')
-                    tabKeyDownFirst.focus();
-                    Array.prototype.forEach.call(tabPanels, function (tabPanel) {
-                        addClass(tabPanel, panelHiddenClass)
-                    })
-                    removeClass(paneKeyDownFirst, panelHiddenClass)
-                    addClass(tabKeyDownFirst.parentNode, listItemSelectedClass)
-                    break;
-                case 39:
-                    Array.prototype.forEach.call(tabLinks, function (tabLink) {
-                        removeClass(tabLink, tabSelectedClass)
-                        removeClass(tabLink.parentNode, listItemSelectedClass)
-                        tabLink.setAttribute('aria-selected', 'false')
-                        tabLink.setAttribute('tabindex', '-1')
-                        tabLink.setAttribute('role', 'tab')
-                    })
-                    addClass(tabKeyDownSecond, tabSelectedClass)
-                    tabKeyDownSecond.setAttribute('aria-selected', 'true')
-                    tabKeyDownSecond.setAttribute('tabindex', '0')
-                    tabKeyDownSecond.focus();
-                    Array.prototype.forEach.call(tabPanels, function (tabPanel) {
-                       addClass(tabPanel, panelHiddenClass)
-                    })
-                    removeClass(paneKeyDownSecond, panelHiddenClass)
-                    addClass(tabKeyDownSecond.parentNode, listItemSelectedClass)
-                    break;
-            }
-
+           if(window.devicePixelRatio < 2) {
+            tabKeyPress(this)
+        }
         })
 
       })
 
 
       function tabClick(tab) {
-
+        var inProgess_tab = document.getElementById("tab_inProgress");
+        var completed_tab = document.getElementById("tab_completed");
         //remove all tabSelectedClass from all tabs
         //change aria-selected to false for all tabs
         Array.prototype.forEach.call(tabLinks, function (tabLink) {
-          removeClass(tabLink, tabSelectedClass)
           removeClass(tabLink.parentNode, listItemSelectedClass)
+          removeClass(tabLink, tabSelectedClass)
           tabLink.setAttribute('aria-selected', 'false')
           tabLink.setAttribute('tabindex', '-1')
           tabLink.setAttribute('role', 'tab')
+
         })
 
         //add selected class to the selected tab
@@ -133,6 +97,9 @@
         addClass(tab, tabSelectedClass)
         tab.setAttribute('aria-selected', 'true')
         tab.setAttribute('tabindex', '0')
+        // Adding the aria-controls to the tab links on page load
+        inProgess_tab.setAttribute("aria-controls", "inProgress");
+        completed_tab.setAttribute("aria-controls", "completed");
 
 
         //hide all the panels
@@ -147,28 +114,174 @@
         addClass(tab.parentNode, listItemSelectedClass)
       }
 
+      function tabKeyPress(tab) {
+          var tabKeyDownFirst = d.getElementsByClassName('govuk-tabs__tab').item(0);
+          var tabKeyDownSecond = d.getElementsByClassName('govuk-tabs__tab').item(1);
+          var paneKeyDownFirst  = d.getElementsByClassName('govuk-tabs__panel').item(0);
+          var paneKeyDownSecond  = d.getElementsByClassName('govuk-tabs__panel').item(1);
+          var inProgess_tab = document.getElementById("tab_inProgress");
+          var completed_tab = document.getElementById("tab_completed");
+          switch (event.keyCode) {
+                case 37:
+                    Array.prototype.forEach.call(tabLinks, function (tabLink) {
+                        removeClass(tabLink, tabSelectedClass)
+                        removeClass(tabLink.parentNode, listItemSelectedClass)
+                        tabLink.setAttribute('aria-selected', 'false')
+                        tabLink.setAttribute('tabindex', '-1')
+                        tabLink.setAttribute('role', 'tab')
+                    })
+                    addClass(tabKeyDownFirst, tabSelectedClass)
+                    tabKeyDownFirst.setAttribute('aria-selected', 'true')
+                    tabKeyDownFirst.setAttribute('tabindex', '0')
+                    tabKeyDownFirst.focus();
+                    inProgess_tab.setAttribute("aria-controls", "inProgress");
+                    completed_tab.setAttribute("aria-controls", "completed");
+                    Array.prototype.forEach.call(tabPanels, function (tabPanel) {
+                        addClass(tabPanel, panelHiddenClass)
+                    })
+                    removeClass(paneKeyDownFirst, panelHiddenClass)
+                    addClass(tabKeyDownFirst.parentNode, listItemSelectedClass)
+                    break;
+                case 40:
+                    Array.prototype.forEach.call(tabLinks, function (tabLink) {
+                         removeClass(tabLink, tabSelectedClass)
+                         removeClass(tabLink.parentNode, listItemSelectedClass)
+                         tabLink.setAttribute('aria-selected', 'false')
+                         tabLink.setAttribute('tabindex', '-1')
+                         tabLink.setAttribute('role', 'tab')
+                    })
+                    addClass(tabKeyDownSecond, tabSelectedClass)
+                    tabKeyDownSecond.setAttribute('aria-selected', 'true')
+                    tabKeyDownSecond.setAttribute('tabindex', '0')
+                    tabKeyDownSecond.focus();
+                    inProgess_tab.setAttribute("aria-controls", "inProgress");
+                    completed_tab.setAttribute("aria-controls", "completed");
+                    Array.prototype.forEach.call(tabPanels, function (tabPanel) {
+                         addClass(tabPanel, panelHiddenClass)
+                    })
+                    removeClass(paneKeyDownSecond, panelHiddenClass)
+                    addClass(tabKeyDownSecond.parentNode, listItemSelectedClass)
+                    break;
+                case 39:
+                    Array.prototype.forEach.call(tabLinks, function (tabLink) {
+                         removeClass(tabLink, tabSelectedClass)
+                         removeClass(tabLink.parentNode, listItemSelectedClass)
+                         tabLink.setAttribute('aria-selected', 'false')
+                         tabLink.setAttribute('tabindex', '-1')
+                         tabLink.setAttribute('role', 'tab')
+                    })
+                    addClass(tabKeyDownSecond, tabSelectedClass)
+                    tabKeyDownSecond.setAttribute('aria-selected', 'true')
+                    tabKeyDownSecond.setAttribute('tabindex', '0')
+                    tabKeyDownSecond.focus();
+                    inProgess_tab.setAttribute("aria-controls", "inProgress");
+                    completed_tab.setAttribute("aria-controls", "completed");
+                    Array.prototype.forEach.call(tabPanels, function (tabPanel) {
+                         addClass(tabPanel, panelHiddenClass)
+                    })
+                    removeClass(paneKeyDownSecond, panelHiddenClass)
+                    addClass(tabKeyDownSecond.parentNode, listItemSelectedClass)
+                    break;
+                case 38:
+                    Array.prototype.forEach.call(tabLinks, function (tabLink) {
+                         removeClass(tabLink, tabSelectedClass)
+                         removeClass(tabLink.parentNode, listItemSelectedClass)
+                         tabLink.setAttribute('aria-selected', 'false')
+                         tabLink.setAttribute('tabindex', '-1')
+                         tabLink.setAttribute('role', 'tab')
+                    })
+                    addClass(tabKeyDownFirst, tabSelectedClass)
+                    tabKeyDownFirst.setAttribute('aria-selected', 'true')
+                    tabKeyDownFirst.setAttribute('tabindex', '0')
+                    inProgess_tab.setAttribute("aria-controls", "inProgress");
+                    completed_tab.setAttribute("aria-controls", "completed");
+                    tabKeyDownFirst.focus();
+                    Array.prototype.forEach.call(tabPanels, function (tabPanel) {
+                         addClass(tabPanel, panelHiddenClass)
+                    })
+                    removeClass(paneKeyDownFirst, panelHiddenClass)
+                    addClass(tabKeyDownFirst.parentNode, listItemSelectedClass)
+                              break;
+          }
+      }
+
     })
-  }
+}
 
 })(document,window);
 
 window.addEventListener("load", function(event) {
     var errorSummary = document.getElementById("error-summary-display");
     var errorSummarylinkHref = document.querySelector("[href='#manage']");
+    var skiplinkTag = document.querySelector("[href='#content']");
     var inProgess_tab = document.getElementById("tab_inProgress");
     var completed_tab = document.getElementById("tab_completed");
-    var skiplinkTag = document.querySelector("[href='#content']");
+    var deleteCLass =  document.querySelectorAll('.govuk-tabs__tab');
     if(errorSummary) errorSummary.focus();
     // This changes the error summary link's href from #maage to #dd
     // This was done to focus the error summary link on the first radio button on the manage_or_track.scala.html page
     if(errorSummarylinkHref) errorSummarylinkHref.setAttribute('href', '#dd');
-    // Adding the aria-controls to the tab links on page load
-    if(inProgess_tab) inProgess_tab.setAttribute("aria-controls", "inProgress");
-    if(completed_tab ) completed_tab.setAttribute("aria-controls", "completed");
     // Changing the skiplink href to bypass the breadcrumbs main-message
     if(skiplinkTag) skiplinkTag.setAttribute('href', '#main-message');
 });
 
 
+function myFunction(x) {
+  var inProgess_tab = document.getElementById("tab_inProgress");
+  var completed_tab = document.getElementById("tab_completed");
+  var sectionInProcess = document.getElementById("inProgress")
+  var sectionCompleted = document.getElementById("completed")
+  var tabSelectedClass = document.getElementsByClassName("govuk-tabs__list-item--selected");
+  var tabHiddenClass = document.getElementsByClassName("govuk-tabs__list-item--selected");
+  if (x.matches) { // If media query matches
+    if(completed_tab) {
+        completed_tab.removeAttribute("aria-controls");
+        completed_tab.removeAttribute("aria-selected");
+        completed_tab.removeAttribute("tabindex");
+        completed_tab.removeAttribute("role");
+    }
+    if(inProgess_tab) {
+        inProgess_tab.removeAttribute("aria-controls");
+        inProgess_tab.removeAttribute("aria-selected");
+        inProgess_tab.removeAttribute("tabindex");
+        inProgess_tab.removeAttribute("role");
+    }
+  }
+}
 
 
+var x = window.matchMedia("(max-width: 641px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
+
+window.addEventListener('resize', function(){
+    var inProgess_tab = document.getElementById("tab_inProgress");
+    var completed_tab = document.getElementById("tab_completed");
+    var sectionInProcess = document.getElementById("inProgress")
+    var sectionCompleted = document.getElementById("completed")
+    var tabSelectedClass = document.getElementsByClassName("govuk-tabs__list-item--selected");
+    var tabHiddenClass = document.getElementsByClassName("govuk-tabs__list-item--selected");
+    if(window.devicePixelRatio >= 2 ) {
+           console.log(window.devicePixelRatio)
+           if(tabSelectedClass) {
+               inProgess_tab.parentNode.classList.remove("govuk-tabs__tab--selected");
+           }
+           if(tabHiddenClass) {
+               inProgess_tab.parentNode.classList.remove("govuk-tabs__panel--hidden")
+           }
+               inProgess_tab.parentNode.removeAttribute("role");
+               sectionInProcess.removeAttribute("role");
+               sectionInProcess.removeAttribute("aria-labelledby");
+
+               completed_tab.parentNode.removeAttribute("role");
+               sectionCompleted.removeAttribute("role");
+               sectionCompleted.removeAttribute("aria-labelledby");
+           if(tabSelectedClass) {
+               completed_tab.parentNode.classList.remove("govuk-tabs__tab--selected");
+           }
+           if(tabHiddenClass) {
+               completed_tab.parentNode.classList.remove("govuk-tabs__panel--hidden")
+           }
+        }
+})
