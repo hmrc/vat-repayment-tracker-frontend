17fb3cf58c03907473d11f8e7bd76dea0c233463
Andy Hicks <455080+andy1138@users.noreply.github.com>
2021-07-23 14:46:14 +0100

    [OPS-6257] remove some dead code

--- app/views/ViewsHelpers.scala
+++ app/views/ViewsHelpers.scala
@@ -18,29 +18,13 @@ package views
 
 
 import uk.gov.hmrc.govukfrontend.views.html.components.FormWithCSRF
-//import uk.gov.hmrc.govukfrontend.views.viewmodels.errorsummary.ErrorSummary
 
 import javax.inject.{Inject, Singleton}
-//import uk.gov.hmrc.play.views.html.helpers._
-//import uk.gov.hmrc.play.views.html.layouts._
 
 @Singleton
 class ViewsHelpers @Inject() (
 
 
     //copied from uk.gov.hmrc.play.views.html.helpers
-//    val errorSummary:       ErrorSummary,
     val form:               FormWithCSRF,
-//    val reportAProblemLink: ReportAProblemLink,
-
-    //copied from uk.gov.hmrc.play.views.html.layouts
-//    val betaBanner:              BetaBanner,
-//    val footer:                  Footer,
-//    val footerLinks:             FooterLinks,
-//    val headWithTrackingConsent: HeadWithTrackingConsent,
-//    val headerNav:               HeaderNav,
-//    val mainContent:             MainContent,
-//    val mainContentHeader:       MainContentHeader,
-//    val serviceInfo:             ServiceInfo,
-//    val sidebar:                 Sidebar
 )
