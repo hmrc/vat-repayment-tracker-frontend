6b2536e5ceb60dd35d13050adeb0d7bc98281cee
Andy Hicks <455080+andy1138@users.noreply.github.com>
2021-07-27 09:58:04 +0100

    [OPS-6257] library upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "0.83.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "0.85.0-play-28",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28"   % "5.6.0",
     "com.beachape" %% "enumeratum"         % "1.6.0",
     "com.beachape" %% "enumeratum-play"    % "1.6.0"
