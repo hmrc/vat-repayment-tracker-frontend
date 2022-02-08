0192cffa1c250e03823621a648ad5366307bb4a4
PucilowskiHMRC <marcin.pucilowski@digital.hmrc.gov.uk>
2022-02-08 09:41:30 +0000

    up component lib

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "1.31.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "3.3.0-play-28",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28"   % "5.20.0",
     "com.beachape" %% "enumeratum"         % "1.6.0",
     "com.beachape" %% "enumeratum-play"    % "1.6.0"
