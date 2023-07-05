ba990ac050f918af3ae4b4a3d6f63a95917d0c7f
Alexis Morcrette <26277447+almorcrette@users.noreply.github.com>
2023-07-05 10:17:19 +0100

    [AM][OPS-10637] update play-frontend-hmrc

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
   val bootstrapVersion = "5.20.0"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "3.3.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.14.0-play-28",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
     "com.beachape" %% "enumeratum"         % "1.6.0",
     "com.beachape" %% "enumeratum-play"    % "1.6.0"
