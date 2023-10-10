3b3f4367acf97d796ae2c21a6f69331bd6765b80
Alexis Morcrette <26277447+almorcrette@users.noreply.github.com>
2023-10-10 15:24:45 +0100

    [AM][OPS-10906] dependency updates:

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile = Seq(
     // FORMAT: OFF
-    "uk.gov.hmrc"  %% "play-frontend-hmrc"         % "7.19.0-play-28",
+    "uk.gov.hmrc"  %% "play-frontend-hmrc"         % "7.23.0-play-28",
     "uk.gov.hmrc"  %% "bootstrap-frontend-play-28" % bootstrapVersion,
     "com.beachape" %% "enumeratum"                 % "1.7.3",
     "com.beachape" %% "enumeratum-play"            % "1.7.3"
@@ -22,7 +22,7 @@ object AppDependencies {
     "com.typesafe.play"      %% "play-test"              % current,
     "org.pegdown"            % "pegdown"                 % "1.6.0" ,
     "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0",
-    "org.wiremock"           % "wiremock-standalone"     % "3.0.4",
+    "org.wiremock"           % "wiremock-standalone"     % "3.2.0",
     "com.vladsch.flexmark"   % "flexmark-all"            % "0.64.8"
     // FORMAT: ON
   ).map(_ % Test)
