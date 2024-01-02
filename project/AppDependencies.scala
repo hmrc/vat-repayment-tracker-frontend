4596dc124d5426c962419b1518a5a5c1bb60afb6
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2024-01-02 15:08:18 +0000

    [AC][OPS-11426] play 3 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,28 +3,26 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "8.1.0"
+  val bootstrapVersion = "8.3.0"
 
   val compile = Seq(
     // FORMAT: OFF
-    "uk.gov.hmrc"   %% "play-frontend-hmrc"         % "7.29.0-play-28",
-    "uk.gov.hmrc"   %% "bootstrap-frontend-play-28" % bootstrapVersion,
+    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "8.2.0",
+    "uk.gov.hmrc"   %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "com.beachape"  %% "enumeratum"                 % "1.7.3",
-    "com.beachape"  %% "enumeratum-play"            % "1.7.3",
+    "com.beachape"  %% "enumeratum-play"            % "1.8.0",
     "org.typelevel" %% "cats-core"                  % "2.10.0"
     // FORMAT: ON
   )
 
   val test = Seq(
     // FORMAT: OFF
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapVersion,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion,
     "org.scalatest"          %% "scalatest"              % "3.2.17",
-    "org.jsoup"              % "jsoup"                   % "1.17.1",
-    "com.typesafe.play"      %% "play-test"              % current,
-    "org.pegdown"            % "pegdown"                 % "1.6.0" ,
-    "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0",
-    "org.wiremock"           % "wiremock-standalone"     % "3.3.1",
-    "com.vladsch.flexmark"   % "flexmark-all"            % "0.64.8"
+    "org.jsoup"              % "jsoup"                   % "1.17.2",
+    "org.playframework"      %% "play-test"              % current,
+    "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.1",
+    "org.wiremock"           % "wiremock-standalone"     % "3.3.1"
     // FORMAT: ON
   ).map(_ % Test)
 
