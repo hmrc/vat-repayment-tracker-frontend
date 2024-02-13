dda32bab2909732799889e100cd249793a54dbaa
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2024-02-13 16:16:30 +0000

    [AC][OPS-11770] library updates (#221)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,11 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "8.3.0"
+  val bootstrapVersion = "8.4.0"
 
   val compile = Seq(
     // FORMAT: OFF
-    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "8.2.0",
+    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "8.5.0",
     "uk.gov.hmrc"   %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "com.beachape"  %% "enumeratum"                 % "1.7.3",
     "com.beachape"  %% "enumeratum-play"            % "1.8.0",
@@ -18,7 +18,7 @@ object AppDependencies {
   val test = Seq(
     // FORMAT: OFF
     "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion,
-    "org.scalatest"          %% "scalatest"              % "3.2.17",
+    "org.scalatest"          %% "scalatest"              % "3.2.18",
     "org.jsoup"              % "jsoup"                   % "1.17.2",
     "org.playframework"      %% "play-test"              % current,
     "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.1",
