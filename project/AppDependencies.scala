7f8c170f65d720c4bec061fb9abf1e98829dbd66
Slawek B. <13876870+slavvco@users.noreply.github.com>
2026-01-29 11:15:39 +0000

    OPS-14492 | Dependency update.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,10 +6,10 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     // FORMAT: OFF
-    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "12.25.0",
+    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "12.29.0",
     "uk.gov.hmrc"   %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "com.beachape"  %% "enumeratum"                 % "1.9.1",
-    "com.beachape"  %% "enumeratum-play"            % "1.9.1",
+    "com.beachape"  %% "enumeratum"                 % "1.9.2",
+    "com.beachape"  %% "enumeratum-play"            % "1.9.2",
     "org.typelevel" %% "cats-core"                  % "2.13.0"
     // FORMAT: ON
   )
@@ -18,7 +18,7 @@ object AppDependencies {
     // FORMAT: OFF
     "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion,
     "org.scalatest"          %% "scalatest"              % "3.2.19",
-    "org.jsoup"              % "jsoup"                   % "1.21.2",
+    "org.jsoup"              % "jsoup"                   % "1.22.1",
     "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.2",
     "org.wiremock"           % "wiremock-standalone"     % "3.13.2"
     // FORMAT: ON
