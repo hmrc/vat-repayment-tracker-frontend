54b05b32bcb02b8a74c20043d156eae53cf82cf3
adambanner1904 <120569454+adambanner1904@users.noreply.github.com>
2026-02-05 13:00:48 +0000

    [AB][OPS-14275] dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,14 @@ import sbt.*
 object AppDependencies {
 
   val bootstrapVersion = "10.5.0"
+  val enumeratumVersion = "1.9.4"
 
   val compile: Seq[ModuleID] = Seq(
     // FORMAT: OFF
-    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "12.29.0",
+    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "12.31.0",
     "uk.gov.hmrc"   %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "com.beachape"  %% "enumeratum"                 % "1.9.2",
-    "com.beachape"  %% "enumeratum-play"            % "1.9.2",
+    "com.beachape"  %% "enumeratum"                 % enumeratumVersion,
+    "com.beachape"  %% "enumeratum-play"            % enumeratumVersion,
     "org.typelevel" %% "cats-core"                  % "2.13.0"
     // FORMAT: ON
   )
@@ -18,7 +19,7 @@ object AppDependencies {
     // FORMAT: OFF
     "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion,
     "org.scalatest"          %% "scalatest"              % "3.2.19",
-    "org.jsoup"              % "jsoup"                   % "1.21.2",
+    "org.jsoup"              % "jsoup"                   % "1.22.1",
     "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.2",
     "org.wiremock"           % "wiremock-standalone"     % "3.13.2"
     // FORMAT: ON
