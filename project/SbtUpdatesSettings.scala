374e3206a695ccb7c91b58e86fa126851ca9f5f7
Adam Banner <120569454+adambanner1904@users.noreply.github.com>
2026-03-18 16:15:05 +0000

    [AB][OPS-14279] GOVUK rebrand (#260)
    * [AB][OPS-14279] GOVUK rebrand
    
    * [AB][OPS-14279] removing 1 unused import
    
    * [AB][OPS-14279] removing a comment
    
    * [AB][OPS-14279] fixing sign out link and service heading link
    
    * [AB][OPS-14279] Exclude play-frontend-hmrc-play-30 from needing to be updated.

--- project/SbtUpdatesSettings.scala
+++ project/SbtUpdatesSettings.scala
@@ -10,6 +10,8 @@ object SbtUpdatesSettings {
     (Compile / compile) := ((Compile / compile) dependsOn dependencyUpdates).value,
     dependencyUpdatesFilter -= moduleFilter("org.scala-lang"),
     dependencyUpdatesFilter -= moduleFilter("org.playframework"),
+    // remove after upgrading to 13.0.0
+    dependencyUpdatesFilter -= moduleFilter("uk.gov.hmrc", "play-frontend-hmrc-play-30"),
     // locked by version of play
     dependencyUpdatesFilter -= moduleFilter("org.scalatestplus.play", "scalatestplus-play")
 
